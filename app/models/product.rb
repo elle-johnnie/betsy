a8987bb760241a6c2025cae386591a0bc2906910
Laura <larmartinez2@gmail.com>
2018-10-18 16:49:39 -0700

    set up relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :prod_name, presence: true, uniqueness: true
-  validates :prod_name, presence: true, numericality: { only_integer: true, greater_than: 0}
+  validates :inv_qty, presence: true, numericality: { only_integer: true, greater_than: 0}
   belongs_to :users
+  has_many :reviews
   has_and_belongs_to_many :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  has_many :products
 end
