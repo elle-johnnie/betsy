fca1703530e18e9a9050a0cd684a3ebd0ed42f47
Laura <larmartinez2@gmail.com>
2018-10-17 17:56:16 -0700

    missed the s in users - following chema

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :prod_name, presence: true, uniqueness: true
   validates :prod_name, presence: true, numericality: { only_integer: true, greater_than: 0}
-  belongs_to :user
+  belongs_to :users
 end
