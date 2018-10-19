fb86bf8b17d5705e5625f1caddabecd0a6b4754b
Jazz <jazziesf21@yahoo.com>
2018-10-18 17:18:14 -0700

    order testing model in progress

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -20,8 +20,9 @@ describe Order do
       expect(order.errors.messages).must_include :cust_name
     end
 
-    it "must be invalid without an email address " do
+    it "must be invalid without an email address or correct data type " do
       order.cust_email = nil
+      order.cust_email = 123
 
       expect(order.cust_email.valid?).must_equal false
       expect(order.errors.messages).must_include :cust_email
@@ -31,7 +32,7 @@ describe Order do
     it "must be invalid without a mailing address" do
       order.mailing_address = nil
 
-      expect(order.description.valid?).must_equal false
+      expect(order.mailing_address.valid?).must_equal false
       expect(order.errors.messages).must_include :mailing_address
     end
 
@@ -43,28 +44,30 @@ describe Order do
       expect(order.errors.messages).must_include :price
     end
 
+    it "must be invalid if date has expired" do
 
-    it "must have a unique name for each order" do
-      order = orders(:one)
-      order2 = orders(:one)
-
-      expect(order2.name.valid?).must_equal false
     end
-  end
+
+    it "must be invalid with more than 16 digits for credit card" do
+
+    end
+
+    it "must have only 3 digits for CVV" do
+
+    end
+
+    it " must be 5 digits in length for zip code" do
+
+    end
 
 
   describe "relations" do
 
-    it "belong to a user" do
-      order = user.order
+    it "belong to a product" do
+      order = product.order
       #add the yml file
-      expect(order).must_be_instance_of User
-      expect(order.id).must_equal user.order_id
+      expect(order).must_be_instance_of Product
+      expect(order.id).must_equal product.order_id
     end
 
-    it "can have a rating" do
-      order = rating.order
-      expect(order).must_be_instance_of Review
-      expect(order.id).must_equal review.order_id
-    end
 end
