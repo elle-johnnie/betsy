60a5bf51bebc89e2488c2504982c34dd5570622d
Trang <trang.frego@gmail.com>
2018-10-19 14:34:45 -0700

    merge conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,9 @@ class Product < ApplicationRecord
   validates :prod_name, presence: true, uniqueness: true
   validates :inv_qty, presence: true, numericality: { only_integer: true, greater_than: 0}
   belongs_to :users
-<<<<<<< HEAD
+
   has_many :reviews
   has_and_belongs_to_many :orders
-=======
 
 
 
@@ -21,6 +20,4 @@ class Product < ApplicationRecord
   end
 
 
-
->>>>>>> b5b2f384e229a73a670fb028ef16ef6a43759ab7
 end
