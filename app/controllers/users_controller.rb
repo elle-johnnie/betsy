588bc8bdfd0eb1599051239db23c1cd26e40631c
Laura <larmartinez2@gmail.com>
2018-10-18 22:53:41 -0700

    removed unnecesary methods and test using ERD, updated create method, and test for it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-  end
-
   # GET /users/1
   def show
   end
@@ -15,43 +10,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
   # POST /users
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    if @user.save # save returns true if the database insert succeeds
+      flash[:success] = 'user Created!'
 
-  # PATCH/PUT /users/1
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+      redirect_to root_path # go to the index so we can see the book in the list
+    else # save failed :(
+      flash.now[:danger] = 'user not created!'
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      render :new, status: :bad_request# show the new book form view again
     end
   end
 
