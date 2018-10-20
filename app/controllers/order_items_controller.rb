56a15d80cd457616eb2e20bca93209050977c015
Trang <trang.frego@gmail.com>
2018-10-19 19:26:59 -0700

    fixed shopping cart create order_item bug

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -7,6 +7,7 @@ class OrderItemsController < ApplicationController
     # check if item is in stock
 
     @order_item = @order.order_items.new(order_item_params)
+    @order_item.save
     @order.save
     session[:order_id] = @order.id
 
@@ -19,7 +20,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order.save
-    
+
     redirect_to cart_path(@order.id)
 
   end
@@ -37,7 +38,7 @@ class OrderItemsController < ApplicationController
 
   def order_item_params
 
-    params.require(:order_item).permit(:qty, product_ids: [])
+    params.require(:order_item).permit(:product_id, :qty)
 
   end
 end
