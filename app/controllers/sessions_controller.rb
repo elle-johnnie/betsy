d140970073d00736fdd89a58fd4009fd4082beae
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-23 21:00:07 -0700

    fixed order validations to on place_order

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:create]
+  skip_before_action :require_login
 
   def create
     auth_hash = request.env['omniauth.auth']
